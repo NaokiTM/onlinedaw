ba8c8c46e0d8f131b5884b6c0ee7a762a8b30032
naoki <ntsome21@gmail.com>
2025-09-16 17:35:18 +0000

    Added menubar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Menubar from "$lib/components/Menubar.svelte";
+</script>
+
+
+<Menubar></Menubar>
+
+<style>
+</style>
