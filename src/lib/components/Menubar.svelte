8479b2999fcf210804c6f6dd085eb4b1025464cb
naoki <ntsome21@gmail.com>
2025-09-16 17:35:18 +0000

    Added menubar component

---
+++ src/lib/components/Menubar.svelte
@@ -0,0 +1,15 @@
+<script>
+// @ts-nocheck
+
+	
+</script>
+
+<nav class="bg-neutral-600 text-white space-x-2 pl-1">
+    <button>File</button>
+    <button>Edit</button>
+    <button>Add</button>
+    <button>View</button>
+    <button>Options</button>
+    <button>Tools</button>
+    <button>Help</button>
+</nav>
