a1004ec580de7e46675e85ae94d6c5ac6e9ffb60
naoki <ntsome21@gmail.com>
2025-09-19 20:04:15 +0100

    formatting to enable scrolling through tracks

--- src/lib/components/track/Tracks.svelte
+++ src/lib/components/track/Tracks.svelte
@@ -6,22 +6,22 @@
     import Trackbody from "$lib/components/track/Trackbody.svelte";
 </script>
 
-
-<!-- map all tracks here -->
-<div class="flex flex-col text-white">
+<div class="flex flex-col text-white flex-1 overflow-auto">
     <div class="flex">
         <div class="flex-2">
-
-            <!-- map track headers here -->
             <Trackoptions></Trackoptions>
+        </div>
+        <div class="flex-8">
+            <Trackbars></Trackbars>            
+        </div>
+    </div>
+    <div class="flex overflow-y-auto">
+        <div class="flex-2">
             {#each $TracksArray as track (track.id)}
                 <Trackheader track={track}></Trackheader>   <!-- pass the track object for each track (may need to pass it to trackbody later for changing track colour on mute etc)-->
             {/each}
-
         </div>
         <div class="flex-8">
-            <!-- map track bodies here -->
-            <Trackbars></Trackbars>  
             {#each $TracksArray}
                 <Trackbody></Trackbody>
             {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
   import Controlbar from "$lib/components/Controlbar.svelte";
   import Menubar from "$lib/components/Menubar.svelte";
-  import Trackoptions from "$lib/components/track/Trackoptions.svelte";
-  import Trackbars from "$lib/components/track/Trackbars.svelte";
-  import Trackheader from "$lib/components/track/Trackheader.svelte";
-  import Trackbody from "$lib/components/track/Trackbody.svelte";
   import Mixingdeck from "$lib/components/Mixingdeck.svelte";
   import Tracks from "$lib/components/track/Tracks.svelte";
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col h-screen select-none">
     <Menubar></Menubar>
     <Controlbar></Controlbar>
     <Tracks></Tracks>
