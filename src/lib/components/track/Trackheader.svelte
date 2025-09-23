cfd2186ec32dc23d17c9d4e0e2db8a5a2769dda1
naoki <ntsome21@gmail.com>
2025-09-23 13:23:29 +0100

    Theming for volume slider in tracks

--- src/lib/components/track/Trackheader.svelte
+++ src/lib/components/track/Trackheader.svelte
@@ -18,12 +18,16 @@
     <img src={track.instrumentIcon} alt='instrumentIcon' class="pb-2 pt-1 pl-1">
     <div class="flex flex-col pt-1">
         <div>{track.instrument}</div>
-        <div class="flex flex-row">
+        <div class="flex flex-row mt-1 space-x-1">
             <button on:click={handleMute} class="hover:cursor-pointer w-5 h-5">
                 <!-- Changes muted icon image depending on whether muted or not -->
                 <img src={track.muted ? muted : unmuted} alt="mute">   
             </button>
-            <input type="range" class="w-30">
+
+            <!-- volume slider -->
+             <div class="">
+                <input type="range" min="1" max="100" value="50" class="w-30 h-3 vol-slider rounded-xl bg-neutral-900">    
+             </div>
         </div>
         <div class="absolute right-0 pr-1 pt-1">
             <div class="flex items-end">
@@ -34,3 +38,34 @@
         </div>
     </div>
 </div>
+
+<style>
+    /* The slider itself */
+    .vol-slider {
+        -webkit-appearance: none;  /* Override default CSS styles */
+        appearance: none;
+        outline: none; /* Remove outline */
+        opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+        -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+        transition: opacity .2s;
+    }
+
+    /* SLIDER HANDLE BELOW: */
+    /* Enables chrome, safari, newer edge styling */
+    .vol-slider::-webkit-slider-thumb {
+        -webkit-appearance: none; /* Override default look */
+        appearance: none;
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        cursor: pointer; /* Cursor on hover */
+        background: #757575;
+        border-radius: 50%;
+    }
+
+    /* Firefox specific styling */
+    .vol-slider::-moz-range-thumb {
+        cursor: pointer; /* Cursor on hover */
+        background: #757575;
+        border-radius: 50%;
+    }
+</style>
