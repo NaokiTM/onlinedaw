c153b43f06ee2023d8f09d7a7e2b83e102fec174
naoki <ntsome21@gmail.com>
2025-12-26 18:41:14 +0000

    Create basic midi editor functionality

--- src/lib/components/editor/EditorTrack.svelte
+++ src/lib/components/editor/EditorTrack.svelte
@@ -1,63 +1,87 @@
+<script lang="ts">
+    import { noOfBars, TracksArray, whichMidiTrackOpen,  } from "$lib/stores";
 
+    let trackIndex: number;
+    $: trackIndex = $whichMidiTrackOpen
+    // let clickedNoteIndex = null
 
-<script>
-    import { noOfBars, whichMidiTrackOpen } from "$lib/stores";
-
-    let trackIndex = whichMidiTrackOpen
-    let clickedNoteIndex = null
-
-    function handleNote() {
+    function handleNote(octaveIndex: number, rowIndex: number, barIndex: number) {
         // if note already exists then remove, if note doesnt exist then add. default length of 1 (fills the bar clicked on)
-        
+        const rowsPerOctave = 8
+        const notePos = octaveIndex * rowsPerOctave + rowIndex;
 
+        TracksArray.update(tracks => {
+            const copy = [...tracks];
+            const track = { ...copy[trackIndex] };
+
+            //Find region for current bar (since we are given the bar index)
+            let region = track.regions.find(r => r.barNo === barIndex);
+
+            //If a region doesnt exist then make a new one 
+            if (!region) {
+                region = {
+                    barNo: barIndex,
+                    length: 1,
+                    notes: []
+                };
+                track.regions = [...track.regions, region];
+            }
+
+            const newNote = {
+                pos: notePos,  //Determines the note the new note is going in
+                length: 1
+            };
+
+            //appends the new note to the existing notes in the region
+            region.notes = [...region.notes, newNote]
+
+            copy[trackIndex] = track;
+            return copy;
+        });
+        //add the note in stores to the track, to a new region length 1, to a bar number, to pos. 
+    }
+
+    //helper function for checking if a note exists at a spot 
+    function hasNoteHelper(octaveIndex: number, barIndex: number, rowIndex: number) {
+        const rowsPerOctave = 8
+        const notePos = octaveIndex * rowsPerOctave + rowIndex
+
+
+        //Get the bar number in the region (if the region exists which it should after clicking a note, then we let it show the note (region))
+        const track = $TracksArray[trackIndex];
+        if (!track) return false;
+
+        const region = track.regions.find(r => r.barNo === barIndex);
+        if (!region) return false;
+
+        //Return true if it finds the matching note 
+        return !!region.notes.find(note => note.pos === notePos)
     }
 </script>
 
+<!-- 8 octaves -->
+{#each Array.from({ length: 12 }) as _, octaveIndex}
 
-{#each Array.from({ length: 8}) as _, i}
-
-    <!-- EVERY 7 NOTES DOWN IT HIGHLIGHTS BORDER RED -->   
-    {#each Array.from({ length: 6}) as _, i}
-        <div class="bg-neutral-900 h-6 border-neutral-600 flex">
-            <!-- loops through bars in tracks -->
-            {#each Array.from({ length: $noOfBars}) as _, i}
-                <div class="border-neutral-800 border-1 w-1/4 h-6 p-0 flex-shrink-0">
-                    <button on:click={handleNote}></button>
-                </div>
-            {/each}
-        </div>
-    {/each}
-
-    <div class="bg-neutral-900 h-6 w-full flex">
-        <!-- loops through bars in tracks -->
-        {#each Array.from({ length: $noOfBars}) as _, i}
-                <div class="border-neutral-800 border-1 border-b-red-900 w-1/4 h-6 p-0 flex-shrink-0">
-                    <button on:click={handleNote}></button>
-                </div>
-        {/each}
-    </div>
-
-    <!-- EVERY 5 NOTES DOWN IT HIGHLIGHTS BORDER RED -->
-    {#each Array.from({ length: 4}) as _, i}
-        <div class="bg-neutral-900 h-6 border-neutral-600 flex">
-            <!-- loops through bars in tracks -->
-            {#each Array.from({ length: $noOfBars}) as _, i}
-                <div class="border-neutral-800 border-1 w-1/4 h-6 p-0 flex-shrink-0">
-                    <button on:click={handleNote}></button>
-                </div>
-            {/each}
-        </div>
-    {/each}
-
+  {#each Array.from({ length: 6 }) as _, rowIndex}
     <div class="bg-neutral-900 h-6 flex">
-        <!-- loops through bars in tracks -->
-        {#each Array.from({ length: $noOfBars}) as _, i}
-            <div class="border-neutral-800 border-1 border-b-red-900 w-1/4 h-6 p-0 flex-shrink-0">
-                <button on:click={handleNote}></button>
-            </div>
-        {/each}
-    </div>
+      {#each Array.from({ length: $noOfBars }) as _, barIndex}
+      
 
+        <div class="border-neutral-800 border-1 w-1/4 h-6">
+          <!-- svelte-ignore a11y_consider_explicit_label -->
+          <!-- svelte-ignore element_invalid_self_closing_tag -->
+          <button
+            class="w-full h-full"
+            class:bg-green-500={hasNoteHelper(octaveIndex, barIndex, rowIndex)}
+            class:bg-neutral-900={!hasNoteHelper(octaveIndex, barIndex, rowIndex)}
+            on:click={() =>
+              handleNote(octaveIndex, rowIndex, barIndex)
+            }
+          />
+        </div>
+      {/each}
+    </div>
+  {/each}
 
 {/each}
 
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -17,9 +17,10 @@ export const TracksArray = writable([
         length: 0,  //Length of the region
         notes: [
           {
-            noteId: 1,   //the id of the note is how many across it is generally. it will be played in sequence of the id
+            //AT THE MOMENT, EACH NOTE FITS EXACTLY ONE BAR. ADD WHEN NOTE LENGTHS CHANGE noteId: 1,   //the id of the note is how many across it is generally. it will be played in sequence of the id
             pos: 1,    //pos is the number of keys down the note is (which note is being played out of the 8 octaves),length is the amount of space it takes  up in a bar
-            length: 1
+            //POS CAN GO UP TO 12 X 8 = 72.  
+            // length: 1
           }
         ],
       },
@@ -60,7 +61,7 @@ export const currentBar = derived(
 
 export const currentBeat = writable(1)   //which beat the caret is above / playing
 export const isPlaying = writable(false)   //Tracks whether the tracks are being played or not (and whether the caret should be moving)
-export const whichMidiTrackOpen = writable(null)  //stores which track is open in the midi editor. 0 if none of them open? 
+export const whichMidiTrackOpen = writable(0)  //stores which track is open in the midi editor. 0 if none of them open? 
 
 //booleans for toggling extra editors
 export const mixingDeckHidden = writable(true)
